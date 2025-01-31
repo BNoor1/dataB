efd0ef9a3a8d3834edf9f7df22aa017c138d73ba
BNoor1 <117393281+BNoor1@users.noreply.github.com>
2025-01-31 21:25:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    // Start the server only after a successful connection
+  
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
